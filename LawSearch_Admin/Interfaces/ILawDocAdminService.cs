21473364c867c9dccd126ff4ee5e076e1d346bcf
Trí Tài <77047496+tritailk65@users.noreply.github.com>
2024-04-08 14:16:32 +0700

    Update API Concept, Keyphrase, LawHTML

--- LawSearch_Admin/Interfaces/ILawDocAdminService.cs
+++ LawSearch_Admin/Interfaces/ILawDocAdminService.cs
@@ -6,7 +6,7 @@ namespace LawSearch_Admin.Interfaces
     public interface ILawDocAdminService
     {
         Task<List<LawDoc>> GetListLawDoc();
-        Task<List<LawHTML>> GetLawHTML(int id);
+        Task<LawHTML> GetLawHTML(int id);
         Task<LawVM> GetDataLaw(int id);
         Task<bool> ImportLaw(string name, string content);
         Task<bool> DeleteLaw(int id);
