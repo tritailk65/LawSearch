087ef55de46f30e3afcff958f69d01df8852f193
Trí Tài <77047496+tritailk65@users.noreply.github.com>
2024-05-16 13:35:09 +0700

    Law item tree view, fix some UI law document

---
+++ LawSearch_Admin/ViewModels/TreeViewItem.cs
@@ -0,0 +1,31 @@
+﻿namespace LawSearch_Admin.ViewModels
+{
+    public class TreeViewItem
+    {
+        public string? Text { get; set; }
+        public string? Type { get; set; }
+        public int? ID { get; set; }
+        public List<TreeViewItem>? Children { get; set; }
+        public bool IsCollapsed { get; set; }
+        public bool IsSelected { get; set; }
+
+        public bool HasChildren => Children != null && Children.Any();
+
+        public TreeViewItem(string? text, string type, int iD, bool isCollapsed = true, bool isSelected = false)
+        {
+            Text = text;
+            Type = type;
+            ID = iD;
+            IsCollapsed = isCollapsed;
+            IsSelected = isSelected;
+        }
+
+        public TreeViewItem(string? text, List<TreeViewItem>? children = null, bool isCollapsed = true, bool isSelected = false)
+        {
+            Text = text;
+            Children = children;
+            IsCollapsed = isCollapsed;
+            IsSelected = isSelected;
+        }
+    }
+}
