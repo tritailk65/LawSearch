e703e76f7b2525f16c75450bb96323581995818d
Nguyen Quoc An <91923556+AnYuku@users.noreply.github.com>
2024-05-19 15:54:04 +0700

    update

---
+++ LawSearch_Core/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using LawSearch_Core.Models;
+
+namespace LawSearch_Core.Interfaces
+{
+    public interface IUserService
+    {
+        User GetUserByID(int id);
+        User CreateNewUser(User user);
+    }
+}
---
+++ LawSearch_Core/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawSearch_Core.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Type { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+            ID = -1;
+            UserName = "DefaultUser";
+            Password = "DefaultPassword";
+            Type = -1;
+            Email = "default@example.com";
+        }
+    }
+}
